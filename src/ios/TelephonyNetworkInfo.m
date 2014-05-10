96740893d0d4a4dedae8e2818c95101a5d3cf117
Thomas Gordon Lowrey IV <linuxinit@gmail.com>
2014-05-09 18:49:10 -0700

    testing

--- src/ios/TelephonyNetworkInfo.m
+++ src/ios/TelephonyNetworkInfo.m
@@ -13,14 +13,14 @@
 	[self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
 
 	[NSNotificationCenter.defaultCenter addObserverForName:CTRadioAccessTechnologyDidChangeNotification 
-		object:nil 
-		queue:nil 
-		usingBlock:^(NSNotification *note) 
-	{
-		NSLog(@"New Radio Access Technology: %@", telephonyInfo.currentRadioAccessTechnology);
-		CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[telephonyInfo currentRadioAccessTechnology]];
-		[self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
-	}];
+													object:nil 
+													 queue:nil 
+												usingBlock:^(NSNotification *note) 
+												{
+													NSLog(@"New Radio Access Technology: %@", telephonyInfo.currentRadioAccessTechnology);
+													CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[telephonyInfo currentRadioAccessTechnology]];
+													[self.commandDelegate sendPluginResult:result callbackId:[command callbackId]];
+												}];
 }
 
 - (void)testServerConnectivity:(CDVInvokedUrlCommand*)command;
@@ -34,6 +34,56 @@
 		CDVPluginResult* result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsBool:([response statusCode]==200)?YES:NO];
 		[self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
 	}];
+}
+
+- (void)onAppTerminate
+{
+	//todo
+	NSLog(@"App terminating");
+}
+
+- (void)onMemoryWarning
+{
+	//todo
+	NSLog(@"Memory Warning");
+}
+
+- (void) onPause 
+{
+	//todo
+	NSLog(@"Pausing");
+}
+
+- (void) onResume 
+{
+	//todo
+	NSLog(@"Resuming");
+}
+
+- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView
+{
+	self = [super init];
+	if (self) {
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onAppTerminate) name:UIApplicationWillTerminateNotification object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onMemoryWarning) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+		[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onReset) name:CDVPluginResetNotification object:theWebView];
+
+		self.webView = theWebView;
+	}
+	return self;
+}
+
+
+- (void)pluginInitialize
+{
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onPause) name:UIApplicationDidEnterBackgroundNotification object:nil];
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onResume) name:UIApplicationWillEnterForegroundNotification object:nil];
+}
+
+- (void)dealloc
+{
+	//cleanup observers
+	[[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
 @end
--- www/network.js
+++ www/network.js
@@ -1,21 +1,21 @@
 
-var exec = require('cordova/exec'),
-    cordova = require('cordova'),
-    channel = require('cordova/channel'),
-    channel2 = require('cordova/channel'),
-    utils = require('cordova/utils');
+var exec     = require('cordova/exec'),
+	cordova  = require('cordova'),
+	channel  = require('cordova/channel'),
+	channel2 = require('cordova/channel'),
+	utils    = require('cordova/utils');
 
 function WirelessNetworkConnection() {
-    this.type = 'unknown';
+	this.type = 'unknown';
 }
 
 WirelessNetworkConnection.prototype.getType = function(successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "WirelessNetworkInfo", "getCurrentAccessTechnology", []);
+	exec(successCallback, errorCallback, "WirelessNetworkInfo", "getCurrentAccessTechnology", []);
 };
 
 //post to URL and check for 200 status
 WirelessNetworkConnection.prototype.testConnectivity = function(url, successCallback, errorCallback) {
-    exec(successCallback, errorCallback, "WirelessNetworkInfo", "testServerConnectivity", [url]);
+	exec(successCallback, errorCallback, "WirelessNetworkInfo", "testServerConnectivity", [url]);
 };
 
 var me = new WirelessNetworkConnection();
@@ -28,59 +28,59 @@ channel2.waitForInitialization('onCordovaConnectionTestReady');
 
 channel.onCordovaReady.subscribe(function() {
 	var timerId = null;
-    me.getType(function(type) {
-        me.type = type;
-        if (type === null){
-            timerId = setTimeout(function(){
-                cordova.fireDocumentEvent("WWANOffline");
-                timerId = null;
-            }, 500);
-        } else {
-            if (timerId !== null) {
-                clearTimeout(timerId);
-                timerId = null;
-            }
-            cordova.fireDocumentEvent("WWANOnline");
-        }
+	me.getType(function(type) {
+		me.type = type;
+		if (type === null){
+			timerId = setTimeout(function(){
+				cordova.fireDocumentEvent("WWANOffline");
+				timerId = null;
+			}, 500);
+		} else {
+			if (timerId !== null) {
+				clearTimeout(timerId);
+				timerId = null;
+			}
+			cordova.fireDocumentEvent("WWANOnline");
+		}
 
-        if (channel.onCordovaWirelessConnectionReady.state !== 2) {
-            channel.onCordovaWirelessConnectionReady.fire();
-        }
-    },
-    function (e) {
-        if (channel.onCordovaWirelessConnectionReady.state !== 2) {
-            channel.onCordovaWirelessConnectionReady.fire();
-        }
-        console.log("Error initializing wireless network connection: " + e);
-    });
+		if (channel.onCordovaWirelessConnectionReady.state !== 2) {
+			channel.onCordovaWirelessConnectionReady.fire();
+		}
+	},
+	function (e) {
+		if (channel.onCordovaWirelessConnectionReady.state !== 2) {
+			channel.onCordovaWirelessConnectionReady.fire();
+		}
+		console.log("Error initializing wireless network connection: " + e);
+	});
 });
 
 channel2.onCordovaReady.subscribe(function() {
 	var timerId = null;
-    me.testConnectivity('http://field-agent.net/api/echo?Message=ping', function(online) {
-        if (online === false){
-            timerId = setTimeout(function(){
-                cordova.fireDocumentEvent("ConnectionTestFailed");
-                timerId = null;
-            }, 500);
-        } else {
-            if (timerId !== null) {
-                clearTimeout(timerId);
-                timerId = null;
-            }
-            cordova.fireDocumentEvent("ConnectionTestSuccess");
-        }
+	me.testConnectivity('http://field-agent.net/api/echo?Message=ping', function(online) {
+		if (online === false){
+			timerId = setTimeout(function(){
+				cordova.fireDocumentEvent("ConnectionTestFailed");
+				timerId = null;
+			}, 500);
+		} else {
+			if (timerId !== null) {
+				clearTimeout(timerId);
+				timerId = null;
+			}
+			cordova.fireDocumentEvent("ConnectionTestSuccess");
+		}
 
-        if (channel2.onCordovaConnectionTestReady.state !== 2) {
-            channel2.onCordovaConnectionTestReady.fire();
-        }
-    },
-    function (e) {
-        if (channel2.onCordovaConnectionTestReady.state !== 2) {
-            channel2.onCordovaConnectionTestReady.fire();
-        }
-        console.log("Error testing wireless connectivity: " + e);
-    });
+		if (channel2.onCordovaConnectionTestReady.state !== 2) {
+			channel2.onCordovaConnectionTestReady.fire();
+		}
+	},
+	function (e) {
+		if (channel2.onCordovaConnectionTestReady.state !== 2) {
+			channel2.onCordovaConnectionTestReady.fire();
+		}
+		console.log("Error testing wireless connectivity: " + e);
+	});
 });
 
 module.exports = me;
